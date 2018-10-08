b55395ab707a759e1b36b70e68677e46b5c4545b
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2018-10-08 15:14:02 -0300

    Agrego logos de secciones

--- js/main.js
+++ js/main.js
@@ -7,32 +7,38 @@ var page = {
 var sections = [
 	{
 		title : "Argentina y el Mundo",
-		description : "Presenta a Argentina en su condición de Estado desde la óptica de la geografía política, proporcionando la información básica necesaria para conocer y comprender la realidad internacional del país, en el pasado y en la actualidad." ,
+		description: "Presenta a Argentina en su condición de Estado desde la óptica de la geografía política, proporcionando la información básica necesaria para conocer y comprender la realidad internacional del país, en el pasado y en la actualidad.",
+		logo: "img/mundo.png",
 		menus: ["Rasgos básicos", "Porciones de territorio", "Límites y fronteras internacionales", "Organización política del territorio", "Formación del territorio", "Malvinas e islas del Atlántico Sur", "Antártida", "Inserción internacional"]
 	},
 	{
 		title: "Argentina físico y natural",
 		description: "Presenta la gran diversidad de condiciones físicas y naturales de Argentina. Esta sección describe las características de los elementos y procesos físico-naturales del territorio argentino.",
+		logo: "img/natural.png",
 		menus: []
 	},
 	{
 		title: "Argentina ambiental",
 		description: "Realiza una aproximación al carácter complejo e interrelacionado del ambiente en el que viven los habitantes de Argentina, dado por las múltiples dimensiones que abarca, tanto en el plano de la realidad social como del sistema natural, y las múltiples combinaciones entre estas dos dimensiones, variables en cada momento y lugar de nuestro país.",
+		logo: "img/ambiental.png",
 		menus: []
 	},
 	{
 		title: "Argentina económica",
 		description: "Describe las características cuantitativas, cualitativas y territoriales de la economía argentina, proporcionando la información necesaria para la comprensión y análisis crítico de su enorme potencial, alcances y limitaciones.",
+		logo: "img/economica.png",
 		menus: []
 	},
 	{
 		title: "Argentina socio-demográfica",
 		description: "Describe las características socio-demográficas más destacadas de los habitantes de Argentina, brindando información detallada y actualizada de la población y la sociedad del país de modo que el usuario pueda comprender su situación y realizar un análisis crítico de la temática.",
+		logo: "img/sociodemo.png",
 		menus: []
 	},
 	{
 		title: "Argentina político-administrativa",
 		description: "Describe el sistema político-administrativo del país y las provincias, proporcionando la información necesaria para la comprensión del sistema de gobierno nacional y provincial y sus principales políticas.",
+		logo: "img/politico_admin.png",
 		menus: []
 	}
 ]
@@ -53,10 +59,15 @@ $(document).ready(function () {
 			$(this).text(sections[i].title);
 			$(this).next().text(sections[i].description);
 		});
+		$('.tab').each(function(i) {
+			$(this).css("background-image", "url(" + sections[i].logo + ")"); // Adds logo within each section's left panel
+		});
 
 		// Load sections into sections tabs
+		/*
 		$(".tab").append("<div class='secciones'></div>");
 		$('.secciones').load( "./secciones.html" );
+		*/
 	} );
 });
 
